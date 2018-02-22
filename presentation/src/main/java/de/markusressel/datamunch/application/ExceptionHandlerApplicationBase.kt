57534e1e6c093aa0f6c1e492f70795d6c1941167
Markus Ressel <markus.ressel@berlin.de>
2018-02-22 02:43:36 +0100

    fixed missing dagger binding

--- presentation/src/main/java/de/markusressel/datamunch/application/ExceptionHandlerApplicationBase.kt
+++ presentation/src/main/java/de/markusressel/datamunch/application/ExceptionHandlerApplicationBase.kt
@@ -19,23 +19,34 @@ abstract class ExceptionHandlerApplicationBase : DaggerApplicationBase() {
         // Set up our own UncaughtExceptionHandler to log errors we couldn't even think of
         Thread
                 .setDefaultUncaughtExceptionHandler { thread, throwable ->
-                    Timber
-                            .e(throwable, "FATAL EXCEPTION")
-
                     try {
+                        Timber
+                                .e(throwable, "FATAL EXCEPTION")
+
                         val timeOfException = Date()
                                 .time
 
-                        //handle non UI thread throw uncaught exception
-                        Handler(Looper.getMainLooper())
-                                .post {
-                                    startActivity(
-                                            UnknownErrorDialog.getNewInstanceIntent(this, throwable,
-                                                                                    timeOfException))
-                                }
+                        if (isUIThread()) {
+                            applicationContext
+                                    .startActivity(UnknownErrorDialog.getNewInstanceIntent(
+                                            applicationContext, throwable, timeOfException))
+                        } else {
+                            //handle non UI thread throw uncaught exception
+                            Handler(Looper.getMainLooper())
+                                    .post {
+                                        applicationContext
+                                                .startActivity(
+                                                        UnknownErrorDialog.getNewInstanceIntent(
+                                                                applicationContext, throwable,
+                                                                timeOfException))
+                                    }
+                        }
                     } catch (e: Exception) {
                         Timber
                                 .e(e, "Error showing \"Unknown Error\" AlertDialog")
+                    } finally {
+                        System
+                                .exit(2) //Prevents the service/app from freezing
                     }
 
                     // not possible without killing all app processes, including the UnkownErrorDialog!?
@@ -43,10 +54,16 @@ abstract class ExceptionHandlerApplicationBase : DaggerApplicationBase() {
                     // Delegates to Android's error handling
                     //                    originalUncaughtExceptionHandler.uncaughtException(thread, throwable);
                     //                }
-
-                    System
-                            .exit(2) //Prevents the service/app from freezing
                 }
+    }
+
+    /**
+     * Check if the current is the UI thread
+     *
+     * @return true if the current thread is the same as the UI thread
+     */
+    private fun isUIThread(): Boolean {
+        return Looper.getMainLooper().thread === Thread.currentThread()
     }
 
 }
--- presentation/src/main/java/de/markusressel/datamunch/dagger/AppModule.kt
+++ presentation/src/main/java/de/markusressel/datamunch/dagger/AppModule.kt
@@ -8,6 +8,7 @@ import dagger.Provides
 import dagger.android.ContributesAndroidInjector
 import de.markusressel.datamunch.application.App
 import de.markusressel.datamunch.view.activity.MainActivity
+import de.markusressel.datamunch.view.activity.UnknownErrorDialog
 import de.markusressel.datamunch.view.activity.base.BottomNavigationActivity
 import de.markusressel.datamunch.view.activity.base.DaggerSupportActivityBase
 import de.markusressel.datamunch.view.activity.base.NavigationDrawerActivity
@@ -33,6 +34,9 @@ abstract class AppModule {
 
     @ContributesAndroidInjector
     internal abstract fun MainActivity(): MainActivity
+
+    @ContributesAndroidInjector
+    internal abstract fun UnknownErrorDialog(): UnknownErrorDialog
 
     @Module
     companion object {
